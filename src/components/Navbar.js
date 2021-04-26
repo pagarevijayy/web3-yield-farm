7b0f61227e251c06f8424109f533008c8cfd0457
pagarevijayy <pagarevj97@gmail.com>
2021-04-26 20:30:15 +0530

    frontend + script

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,10 @@ class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap px-0 py-1 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
+          href="https://github.com/pagarevijayy"
           target="_blank"
           rel="noopener noreferrer"
         >

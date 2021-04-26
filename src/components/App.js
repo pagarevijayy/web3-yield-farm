39f30a5fd51dd240afca243be9d3243bfdbdc0a9
pagarevijayy <pagarevj97@gmail.com>
2021-04-26 21:06:24 +0530

    improved: refresh state

--- src/components/App.js
+++ src/components/App.js
@@ -75,17 +75,38 @@ class App extends Component {
   stakeTokens = (amount) => {
     this.setState({ loading: true })
     this.state.daiToken.methods.approve(this.state.tokenFarm._address, amount).send({ from: this.state.account }).on('transactionHash', (hash) => {
-      this.state.tokenFarm.methods.stakeTokens(amount).send({ from: this.state.account }).on('transactionHash', (hash) => {
-        this.setState({ loading: false })
+      this.state.tokenFarm.methods.stakeTokens(amount).send({ from: this.state.account }).on('receipt', (receipt) => {        
+        this.refreshTokensState();
       })
     })
   }
 
   unstakeTokens = (amount) => {
     this.setState({ loading: true })
-    this.state.tokenFarm.methods.unstakeTokens().send({ from: this.state.account }).on('transactionHash', (hash) => {
-      this.setState({ loading: false })
+    this.state.tokenFarm.methods.unstakeTokens().send({ from: this.state.account }).on('receipt', (receipt) => {
+      this.refreshTokensState();
     })
+  }
+
+  async refreshTokensState() {
+    // fetch updated balance
+    let updatedDaiBalance = await this.state.daiToken.methods.balanceOf(this.state.account).call();
+
+    let updatedDappBalance = await this.state.dappToken.methods.balanceOf(this.state.account).call();
+
+    let updatedStakingBalance = await this.state.tokenFarm.methods.stakingBalance(this.state.account).call()
+
+
+    // update the state
+    this.setState(
+      {
+        daiTokenBalance: updatedDaiBalance.toString(),
+        dappTokenBalance: updatedDappBalance.toString(),
+        stakingBalance: updatedStakingBalance.toString(),
+        loading: false
+      }
+    );
+
   }
 
   constructor(props) {
